2a22c0d95f025e189aa625570880caba2cb21010
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-11-18 16:00:41 +0100

    Fix pdfbox path

--- find_java_files.sh
+++ find_java_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find pfdbox/src/. -type f -name "*.java" > java_paths.txt
+find pdfbox/pdfbox/src/. -type f -name "*.java" > java_paths.txt
 rm -r sonarqube-repair/source/act/ABC
-mkdir sonarqube-repair/source/act/ABC
+mkdir -p sonarqube-repair/source/act/ABC
 while read java_file; do
     cp "$java_file" sonarqube-repair/source/act/ABC
 done <java_paths.txt
