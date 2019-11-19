21755861aa499de5ba54de402661923e822a7b71
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-11-19 13:01:17 +0100

    Move everything to one script and apply processor to TraceSortList

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-hub clone kth-tcs/sonarqube-repair
-cd sonarqube-repair
-mvn clean package
+#hub clone kth-tcs/sonarqube-repair
+#cd sonarqube-repair
+#mvn clean package
+#cd ..
+# hub clone apache/commons-cli
+#find commons-cli/src/. -type f -name "*.java" > java_paths.txt
+hub clone nharrand/TraceSortList
+cd TraceSortList
+git checkout -b BigDecimalDoubleConstructor
+find src -type f -name "*.java" > java_paths.txt
+mv java_paths.txt ../
+#rm -r sonarqube-repair/source/act/apache/commons/cli
 cd ..
-hub clone apache/commons-cli
+rm -r sonarqube-repair/source/act/TraceSortList
+mkdir -p sonarqube-repair/source/act/TraceSortList
+while read java_file; do
+        cp TraceSortList/"$java_file" sonarqube-repair/source/act/TraceSortList
+done <java_paths.txt
+#rm java_paths.txt
+cd sonarqube-repair
+java -cp target/sonarqube-repair-0.1-SNAPSHOT-jar-with-dependencies.jar Main 2272
