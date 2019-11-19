2ef256e3278ca68b8937d85ae06dd94b8b2c34bd
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-11-19 15:50:43 +0100

    Some additoins, will rewrite in python

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#hub clone kth-tcs/sonarqube-repair
-#cd sonarqube-repair
-#mvn clean package
-#cd ..
+project=TraceSortList
+sourceFolder=sonarqube-repair/source/act/$project
+hub clone kth-tcs/sonarqube-repair
+cd sonarqube-repair
+mvn clean package
+cd ..
 # hub clone apache/commons-cli
 #find commons-cli/src/. -type f -name "*.java" > java_paths.txt
-hub clone nharrand/TraceSortList
-cd TraceSortList
+hub clone nharrand/$project
+cd $project
 git checkout -b BigDecimalDoubleConstructor
-find src -type f -name "*.java" > java_paths.txt
-mv java_paths.txt ../
+find src -type f -name "*.java" > ../java_paths.txt
 #rm -r sonarqube-repair/source/act/apache/commons/cli
 cd ..
-rm -r sonarqube-repair/source/act/TraceSortList
-mkdir -p sonarqube-repair/source/act/TraceSortList
+rm -r $sourceFolder
+mkdir -p $sourceFolder
 while read java_file; do
-        cp TraceSortList/"$java_file" sonarqube-repair/source/act/TraceSortList
+        cp $project/"$java_file" $sourceFolder
 done <java_paths.txt
 #rm java_paths.txt
 cd sonarqube-repair
 java -cp target/sonarqube-repair-0.1-SNAPSHOT-jar-with-dependencies.jar Main 2272
+find sonarqube-repair/spooned/se -type f -name "*.java" > spooned_files.txt
+find TraceSortList/src/main/java/se -type f -name "*.java" && find TraceSortList/src/test/java/se -type f -name "*.java" > original_files.txt
