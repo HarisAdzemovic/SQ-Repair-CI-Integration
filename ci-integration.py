867502df57d618e9b33ccab10996b5a781bfc689
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-11-19 15:58:20 +0100

    Transforms all TraceSortList files

--- ci-integration.py
+++ ci-integration.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-import subprocess;
+import subprocess as sp;
 import os;
 import shutil;
 
 sourceFolder = "sonarqube-repair/source/act/TraceSortList";
-subprocess.call('hub clone kth-tcs/sonarqube-repair', shell = True);
-subprocess.call('mvn clean package', shell = True, cwd = "sonarqube-repair");
-subprocess.call('hub clone nharrand/TraceSortList', shell = True);
-subprocess.call('git checkout -b IteratorNextException', shell = True, cwd = "TraceSortList");
+#sp.call('hub clone kth-tcs/sonarqube-repair', shell = True);
+#sp.call('mvn clean package', shell = True, cwd = "sonarqube-repair");
+#sp.call('hub clone nharrand/TraceSortList', shell = True);
+#sp.call('git checkout -b IteratorNextException', shell = True, cwd = "TraceSortList");
 path = "TraceSortList"
 originalFiles = [];
 for r, d, f in os.walk(path):
@@ -19,7 +19,12 @@ for f in originalFiles:
     print(f)
 
 try:
-	shutil.rmtree(sourceFolder, ignore_errors=True);
+    shutil.rmtree(sourceFolder, ignore_errors=True);
 except OSError:
-	print ("Directory does not exist");
+    print ("Directory does not exist");
 os.makedirs(sourceFolder);
+
+for f in originalFiles:
+    shutil.copy(f, sourceFolder);
+
+sp.call('java -cp target/sonarqube-repair-0.1-SNAPSHOT-jar-with-dependencies.jar Main 2272', shell = True, cwd = "sonarqube-repair")
